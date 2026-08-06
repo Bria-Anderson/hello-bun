3b98d0d7f803b3ccbcc670752cbe021906afbf21
Bria <janae.anderson92@icloud.com>
2026-08-05 21:16:09 -0500

    start hello-bun with hello script and setup notes

---
+++ hello.js
@@ -0,0 +1,6 @@
+let course = "Level 2";
+let message = "Hello from " + course + "!";
+let display = "Bria";
+console.log(message);
+console.log("This JavaScript runs on My machine.");
+console.log("My name is " + display + ".");
