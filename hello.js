9b21d492b73d1567bc223e467765ef9e73ae2170
Bria <janae.anderson92@icloud.com>
2026-08-07 21:21:23 -0500

    changed message hello.js prints

--- hello.js
+++ hello.js
@@ -4,4 +4,4 @@ let display = "Bria";
 console.log(message);
 console.log("This JavaScript runs on My machine.");
 console.log("My name is " + display + ".");
-console.log("8/6/2026");
+console.log("8/7/2026");
